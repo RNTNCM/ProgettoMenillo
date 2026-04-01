50ebefb23ed75102ca3bb03daa5caa9a25a97085
lisamancuso <lisa.mancuso@edu.immaginazioneelavoro.it>
2026-04-01 11:55:19 +0200

    Update creazione_db.sql

--- Lisa/creazione_db.sql
+++ Lisa/creazione_db.sql
@@ -41,7 +41,7 @@ values ( 1,'2026-05-01', '2026-05-21', '09:00 - 13:00', 'Torino', 50, 34),
 ( 5,'2026-06-08', '2026-06-19', '09:00 - 12:00', 'Chieri', 60, 24),
 ( 5,'2026-06-08', '2026-06-19', '14:00 - 17:00', 'Chieri', 50, 39);
 
-create user 'admin'@'localhost' identified by 'admin';
-grant select on corsi_aziendali.* to 'admin'@'localhost';
+create user 'admin_corsi_aziendali'@'localhost' identified by 'admin';
+grant select on corsi_aziendali.* to 'admin_corsi_aziendali'@'localhost';
 
 flush privileges;
